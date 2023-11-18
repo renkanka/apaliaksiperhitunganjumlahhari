be63c475a495f051692f50016ed6438af2612312
veerr <117021162+Raja706@users.noreply.github.com>
2023-11-18 21:40:47 +0800

    first commit

---
+++ src/aplikasi/penghitungan/juamlah/har/integer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasi.penghitungan.juamlah.har;
+
+/**
+ *
+ * @author WINDOWS
+ */
+class integer {
+
+    static int valueOF(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
